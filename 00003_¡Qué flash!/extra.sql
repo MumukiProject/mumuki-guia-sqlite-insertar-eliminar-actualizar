ad4eb5402e678244c97d354c043802cce0003f84
nadia@mumuki.org <nadia@mumuki.org>
2018-01-08 12:20:57 -0500

    Mumuki Export on 2018-01-08 12:20:57 -0500

--- 00003_¡Qué flash!/extra.sql
+++ 00003_¡Qué flash!/extra.sql
@@ -1,1 +1,3 @@
 CREATE TABLE series_peliculas (id_contenido INTEGER PRIMARY KEY, titulo TEXT, descripcion TEXT, creador TEXT, principales TEXT, temporadas INTEGER, estreno INTEGER);
+
+INSERT INTO series_peliculas VALUES (1, "Stranger things", "Después de la extraña desaparición de un niño, un pueblo se encuentra ante un misterio que revela experimentos secretos, fuerzas sobrenaturales y a una niña muy especial.", "The Duffer Brothers", "Eleven, Mike, Will, Dustin, Lucas, Hopper, Joyce, Nancy, Jonathan, Steve", 2, 2016), (2, "Breaking bad", "Un profesor de química de escuela secundaria recurre a la venta de drogas para mantener a su familia.", "Vince Gilligan", "Walter White, Jesse Pinkman, Gus Fring, Saul Goodman, Mike Ehrmantraut, Hank Schrader, Tuco Salamanca, Skyler White", 5, 2008);
