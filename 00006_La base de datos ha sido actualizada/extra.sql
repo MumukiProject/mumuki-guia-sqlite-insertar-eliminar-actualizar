99dad7efa4e335e7071583d0776bd902c14dd094
nadia@mumuki.org <nadia@mumuki.org>
2018-01-08 13:08:20 -0500

    Mumuki Export on 2018-01-08 13:08:20 -0500

--- 00006_La base de datos ha sido actualizada/extra.sql
+++ 00006_La base de datos ha sido actualizada/extra.sql
@@ -1,3 +1,3 @@
 CREATE TABLE series_peliculas (id_contenido INTEGER PRIMARY KEY, titulo TEXT, puntuacion INTEGER);
 
-INSERT INTO series_peliculas VALUES (1, "Stranger things", 8), (2, "Breaking bad", 8),  (3, "IT", 8), (4, "Better call Saul", 8), (5, "The Flash", 8);
+INSERT INTO series_peliculas VALUES (1, "Stranger things", 9.7), (2, "Breaking bad", 7),  (3, "IT", 4.9), (4, "Better call Saul", 6), (5, "The Flash", 4.7);
