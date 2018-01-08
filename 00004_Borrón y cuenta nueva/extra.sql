307525fae9b02567504dd8a29987525b290eb9b3
nadia@mumuki.org <nadia@mumuki.org>
2018-01-08 12:41:18 -0500

    Mumuki Export on 2018-01-08 12:41:18 -0500

--- 00004_Borrón y cuenta nueva/extra.sql
+++ 00004_Borrón y cuenta nueva/extra.sql
@@ -1,3 +1,6 @@
 CREATE TABLE series_peliculas (id_contenido INTEGER PRIMARY KEY, titulo TEXT, descripcion TEXT, creador TEXT, principales TEXT, temporadas INTEGER, estreno INTEGER);
 
 INSERT INTO series_peliculas VALUES (1, "Stranger things", "Después de la extraña desaparición de un niño, un pueblo se encuentra ante un misterio que revela experimentos secretos, fuerzas sobrenaturales y a una niña muy especial.", "The Duffer Brothers", "Eleven, Mike, Will, Dustin, Lucas, Hopper, Joyce, Nancy, Jonathan, Steve", 2, 2016);
+
+create table lala (id_contenido INTEGER PRIMARY KEY, titulo TEXT);
+insert into lala VALUES (1, "lalala");
